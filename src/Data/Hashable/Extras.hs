3be4c62ec274610fbc9a16f7a3afecf07bbdb3b6
Edward Kmett <ekmett@gmail.com>
2013-03-21 19:27:27 -0400

    Added a module header

--- src/Data/Hashable/Extras.hs
+++ src/Data/Hashable/Extras.hs
@@ -1,4 +1,13 @@
 {-# LANGUAGE DefaultSignatures #-}
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) Edward Kmett 2013
+-- License   :  BSD3
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
 module Data.Hashable.Extras
   ( Hashable1(..)
   , Hashable2(..)
